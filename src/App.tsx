b5d7e22f12a1f8e13ae5b03a5e3c100b18a16d1c
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-03 11:17:45 +0000

    HU-1 Pagina de presentacion creada

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { PresentationPage } from "./pages/PresentationPage";
 
 function App() {
   return (
     <div className="App">
-      <p className="text-3xl font-bold underline p-10">Hola</p>
+      <PresentationPage />
     </div>
   );
 }
---
+++ src/pages/PresentationPage.tsx
@@ -0,0 +1,8 @@
+
+export const PresentationPage = () => {
+    return (
+        <div>
+            hola 2
+        </div>
+    )
+}
