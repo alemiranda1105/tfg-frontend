a4f37f1590e0ab5686bcdb65b557c9182a7f4e2d
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-03 11:27:47 +0000

    HU-1 Template pagina de presentacion

--- src/pages/PresentationPage.tsx
+++ src/pages/PresentationPage.tsx
@@ -1,8 +1,22 @@
+import { characteristicsText, datasetText, descriptionText } from "../mock/MockedTextPresentation"
 
 export const PresentationPage = () => {
     return (
         <div>
-            hola 2
+            <div>
+                <h2>Descripción</h2>
+                <p>
+                    {descriptionText}
+                </p>
+                <h2>Características</h2>
+                <p>
+                    {characteristicsText}
+                </p>
+                <h2>Acerca de nuestro dataset</h2>
+                <p>
+                    {datasetText}
+                </p>
+            </div>
         </div>
     )
-}
+};
