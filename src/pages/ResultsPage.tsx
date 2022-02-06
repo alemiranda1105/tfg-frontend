aa81e6bbbc6f21a0c057e096ace5a796f40dd9cd
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-06 11:04:33 +0000

    HU-3 Diseño de tabla creado

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -1,9 +1,13 @@
+import { MethodsTableComponent } from "../components/MethodsTableComponent"
 
 export const ResultsPage = () => {
     return (
-        <div className="h-screen">
-            <h1>Resultados y ranking</h1>
-            <h3>Aquí podrá visualizar todos los resultados de los métodos subidos por los usuarios</h3>
+        <div className="h-screen flex flex-col items-center">
+            <div className="flex flex-col items-center w-[80%] m-8 p-4 rounded-md drop-shadow bg-white">
+                <h1 className="text-4xl font-extrabold">Resultados y ranking</h1>
+                <h3 className="text-xl font-light">Aquí podrá visualizar todos los resultados de los métodos subidos por los usuarios</h3>
+            </div>
+            <MethodsTableComponent />
         </div>
     )
 }
