1dbe6f3eec7c8c83bb93f29a07727c3b64716f4b
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-06 11:34:12 +0000

    HU-3 carga de los elementos

--- src/components/MethodsTableComponent.tsx
+++ src/components/MethodsTableComponent.tsx
@@ -9,29 +9,40 @@ export const MethodsTableComponent = () => {
     });
 
     return (
-        <table className="border-collapse min-w-full">
-            <thead className="bg-blue-100">
-                <tr>
-                    <th className="py-4 px-6 text-left">Nombre</th>
-                    <th className="py-4 px-6 text-left">M1</th>
-                    <th className="py-4 px-6 text-left">M2</th>
-                    <th className="py-4 px-6 text-left">M3</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr className="border-b">
-                    <td className="py-4 px-6 font-semibold whitespace-nowrap">Metodo 1</td>
-                    <td className="py-4 px-6 font-semibold whitespace-nowrap">0.1215</td>
-                    <td className="py-4 px-6 font-semibold whitespace-nowrap">0.6778</td>
-                    <td className="py-4 px-6 font-semibold whitespace-nowrap">0.7627</td>
-                </tr>
-                <tr className="border-b">
-                    <td className="py-4 px-6 font-semibold whitespace-nowrap">Metodo 2</td>
-                    <td className="py-4 px-6 font-semibold whitespace-nowrap">0.5241</td>
-                    <td className="py-4 px-6 font-semibold whitespace-nowrap">0.9663</td>
-                    <td className="py-4 px-6 font-semibold whitespace-nowrap">0.4775</td>
-                </tr>
-            </tbody>
-        </table>
+        <>
+        {
+            isPending &&
+            <div className="flex flex-col items-center">
+                <h3 className="animate-pulse text-2xl font-bold">Cargando...</h3>
+            </div>
+        }
+        {
+            !isPending &&
+            <table className="border-collapse min-w-full">
+                <thead className="bg-blue-100">
+                    <tr>
+                        <th className="py-4 px-6 text-left">Nombre</th>
+                        <th className="py-4 px-6 text-left">M1</th>
+                        <th className="py-4 px-6 text-left">M2</th>
+                        <th className="py-4 px-6 text-left">M3</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="border-b">
+                        <td className="py-4 px-6 font-semibold whitespace-nowrap">Metodo 1</td>
+                        <td className="py-4 px-6 font-semibold whitespace-nowrap">0.1215</td>
+                        <td className="py-4 px-6 font-semibold whitespace-nowrap">0.6778</td>
+                        <td className="py-4 px-6 font-semibold whitespace-nowrap">0.7627</td>
+                    </tr>
+                    <tr className="border-b">
+                        <td className="py-4 px-6 font-semibold whitespace-nowrap">Metodo 2</td>
+                        <td className="py-4 px-6 font-semibold whitespace-nowrap">0.5241</td>
+                        <td className="py-4 px-6 font-semibold whitespace-nowrap">0.9663</td>
+                        <td className="py-4 px-6 font-semibold whitespace-nowrap">0.4775</td>
+                    </tr>
+                </tbody>
+            </table>
+        }
+        </>
     );
 }
