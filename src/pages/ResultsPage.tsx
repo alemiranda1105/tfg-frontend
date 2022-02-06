69932930151952cfb9c27e8caabf4f715a73534b
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-06 10:22:42 +0000

    HU-3 Pagina de resultados

---
+++ src/pages/ResultsPage.tsx
@@ -0,0 +1,9 @@
+
+export const ResultsPage = () => {
+    return (
+        <div className="h-screen">
+            <h1>Resultados y ranking</h1>
+            <h3>Aquí podrá visualizar todos los resultados de los métodos subidos por los usuarios</h3>
+        </div>
+    )
+}
