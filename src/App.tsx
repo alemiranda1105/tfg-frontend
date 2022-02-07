3e740b182cfc2282cd509a892ee5b5eec9ba2291
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-07 16:11:03 +0000

    Añadida barra navegacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { FooterComponent } from "./components/FooterComponent";
+import { NavigationBar } from "./components/NavigationBar";
 import { DownloadingPage } from "./pages/DownloadingPage";
 import { PresentationPage } from "./pages/PresentationPage";
 import { ResultsPage } from "./pages/ResultsPage";
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App font-roboto">
       <BrowserRouter>
+        <NavigationBar />
         <Routes>
           <Route path="/" element={<PresentationPage />} />
           <Route path="/results" element={<ResultsPage />} />
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,9 @@
+
+
+export const NavigationBar = () => {
+    return (
+        <div>
+            navigation
+        </div>
+    )
+}
