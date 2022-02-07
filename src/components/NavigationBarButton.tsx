8b3968e4c60802396e461b189e321ae394eea0f4
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-07 18:55:44 +0000

    Botones de navegacion mejorados

--- src/components/NavigationBarButton.tsx
+++ src/components/NavigationBarButton.tsx
@@ -5,7 +5,7 @@ export const NavigationBarButton = ({name, url}: LinkDict) => {
     return(
         <>
             <Link 
-            className="font-bold p-2.5 mx-5 hover:scale-110 hover:my-2 hover:bg-blue-500 hover:rounded hover:shadow hover:text-white duration-150" 
+            className="font-bold p-2.5 mx-3 my-1 hover:bg-blue-400 hover:rounded hover:shadow-inner hover:text-white duration-150" 
             to={url}>
                 {name}
             </Link>
