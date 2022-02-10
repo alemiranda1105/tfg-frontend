406897c5db4c8b738f4345e152451d0b8a3569b7
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-10 19:14:48 +0000

    Method model updated

--- src/components/MethodDetailsComponent.tsx
+++ src/components/MethodDetailsComponent.tsx
@@ -1,6 +1,26 @@
+import { v4 } from "uuid";
 import { useFetch } from "../hooks/useFetch";
 import { AuthorDataComponent } from "./AuthorDataComponent";
 import { MethodInterface } from "./MethodsTableComponent";
+
+/* Private component */
+interface ResultTableRowProps {
+    name: string,
+    result: number
+}
+
+function ResultTableRow({name, result}: ResultTableRowProps) {
+    return (
+        <>
+            <tr>
+                <th className="bg-blue-100">{name}</th>
+            </tr>
+            <tr>
+                <td>{result}</td>
+            </tr>
+        </>
+    )
+}
 
 interface MethodDetailsProps {
     methodId: string
@@ -34,47 +54,38 @@ export const MethodDetailsComponent = ({methodId}: MethodDetailsProps) => {
         {
             method && !isPending &&
             <div className="flex flex-col items-center bg-white rounded shadow-md w-11/12 m-3 md:w-4/6">
-            <div className="flex flex-col items-center m-2 w-full">
-                <h3 className="text-xl font-bold">Nombre</h3>
-                <h4 className="text-lg">{method.name}</h4>
-            </div>
+                <AuthorDataComponent id={method.user_id}/>
             
-            <AuthorDataComponent id={method.user_id}/>
+                <div className="flex flex-col items-center m-2 w-full">
+                    <h3 className="text-xl font-bold">Nombre</h3>
+                    <h4 className="text-lg">{method.name}</h4>
+                </div>
 
-            <div className="flex flex-col items-center m-2 w-full">
-                <h3 className="text-xl font-bold">Descripción</h3>
-                <h4 className="text-lg">{method.info}</h4>
+                <div className="flex flex-col items-center m-2 w-full">
+                    <h3 className="text-xl font-bold">Descripción</h3>
+                    <h4 className="text-lg">{method.info}</h4>
+                </div>
+
+                <div className="flex flex-col items-center m-2 w-full">
+                    <h3 className="text-xl font-bold">Link de la publicación</h3>
+                    <h4 className="text-lg">{method.link}</h4>
+                </div>
+
+                <div className="flex flex-col items-center m-2 w-full">
+                    <h3 className="text-xl font-bold">Resultados:</h3>
+                    <table className="text-center border">
+                        <tbody>
+                            {
+                                Object.entries(method.results).map(result => {
+                                    return (
+                                        <ResultTableRow key={v4()} name={result[0]} result={result[1]}/>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div className="flex flex-col items-center m-2 w-full">
-                <h3 className="text-xl font-bold">Link de la publicación</h3>
-                <h4 className="text-lg">{method.link}</h4>
-            </div>
-            <div className="flex flex-col items-center m-2 w-full">
-                <h3 className="text-xl font-bold">Resultados:</h3>
-                <table className="text-center border">
-                    <tbody>
-                        <tr>
-                            <th className="bg-blue-100">f1_score</th>
-                        </tr>
-                        <tr>
-                            <td>{method.results[0].f1_score}</td>
-                        </tr>
-                        <tr>
-                            <th className="bg-blue-100">recall_score</th>
-                        </tr>
-                        <tr>
-                            <td>{method.results[1].recall_score}</td>
-                        </tr>
-                        <tr>
-                            <th className="bg-blue-100">precision_score</th>
-                        </tr>
-                        <tr>
-                            <td>{method.results[2].precision_score}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
         }
         </>
     );
