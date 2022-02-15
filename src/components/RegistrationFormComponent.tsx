075a2846f3e1c6b5bbfdc346434297f5910870d0
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-15 17:44:26 +0000

    HU-8 Welcome component created

--- src/components/RegistrationFormComponent.tsx
+++ src/components/RegistrationFormComponent.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { EmailRegex } from "../helpers/EmailRegex";
+import { WelcomeUserComponent } from "./WelcomeUserComponent";
 
 export interface UserDataInterface {
     email: string, 
@@ -33,8 +34,7 @@ export const RegistrationFormComponent = () => {
         .then(res => res.data)
         .then(data => {
             setLoginError("");
-            setUserData(data)
-            console.log(userData);
+            setUserData(data);
         })
         .catch(error => {
             if(axios.isAxiosError(error)) {
@@ -47,51 +47,54 @@ export const RegistrationFormComponent = () => {
 
     return (
         <div className="flex flex-col items-center w-3/4 p-4 rounded-md drop-shadow bg-white">
-            <form className="flex flex-col items-center w-full" onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-col items-center w-full m-3">
-                    <label htmlFor="username">Nombre de usuario:</label>
-                    <input type="text" id="username" placeholder="Nombre de usuario" 
-                        {...register('username', {
-                                required: { value: true, message: 'Introduzca un nombre de usuario'}, 
-                                maxLength: { value: 20, message: 'El nombre de usuario debe de ser menor de 20 caracteres'}, 
-                                minLength: { value: 3, message: 'El nombre de usuario debe de ser mayor de 3 caracteres'}}
-                            )
-                        }
-                        className="border rounded-md shadow w-full md:w-1/3 py-1 px-2 max-w-xs focus:w-full focus:border focus:border-blue-500 outline-none ease-in-out duration-300"
-                    />
-                    {errors.username?.message && <ErrorValidationText error={errors.username.message}/>}
-                </div>
-                <div className="flex flex-col items-center w-full m-3">
-                    <label htmlFor="email">Correo electrónico:</label>
-                    <input type="email" id="email" placeholder="Email" 
-                        {...register('email', {
-                                required: { value: true, message: 'Introduzca un correo electrónico'},
-                                pattern: { value: EmailRegex, message: 'Introduzca un correo electrónico válido'}}
-                            )
-                        }
-                        className="border rounded-md shadow w-full md:w-1/3 py-1 px-2 max-w-xs focus:w-full focus:border focus:border-blue-500 outline-none ease-in-out duration-300"
-                    />
-                    {errors.email?.message && <ErrorValidationText error={errors.email.message}/>}
-                </div>
-                <div className="flex flex-col items-center w-full m-3">
-                    <label htmlFor="password">Contraseña:</label>
-                    <input type="password" id="password" placeholder="Contraseña" 
-                        {...register('password', {
-                                required: {value: true, message: 'Introduzca una contraseña'},
-                                minLength: {value: 6, message: 'La contraseña es demasiado corta'}}
-                            )
-                        }
-                        className="border rounded-md shadow w-full md:w-1/3 py-1 px-2 max-w-xs focus:w-full focus:border focus:border-blue-500 outline-none ease-in-out duration-300"
-                    />
-                    {errors.password?.message && <ErrorValidationText error={errors.password.message}/>}
-                </div>
-                <div className="flex flex-col items-center w-full mt-5">
-                    <button type="submit" className="px-3 py-2 mb-2 rounded-md bg-blue-500 text-white hover:bg-blue-400 hover:scale-105 hover:shadow-md transition duration-100 ease-in-out active:shadow-none">
-                        <h4 className="text-white font-bold">Registro</h4>
-                    </button>
-                    {loginError && <ErrorValidationText error={loginError} />}
-                </div>
-            </form>
+            {userData && !loginError && <WelcomeUserComponent data={userData} />}
+            {!userData && 
+                <form className="flex flex-col items-center w-full" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="flex flex-col items-center w-full m-3">
+                        <label htmlFor="username">Nombre de usuario:</label>
+                        <input type="text" id="username" placeholder="Nombre de usuario" 
+                            {...register('username', {
+                                    required: { value: true, message: 'Introduzca un nombre de usuario'}, 
+                                    maxLength: { value: 20, message: 'El nombre de usuario debe de ser menor de 20 caracteres'}, 
+                                    minLength: { value: 3, message: 'El nombre de usuario debe de ser mayor de 3 caracteres'}}
+                                )
+                            }
+                            className="border rounded-md shadow w-full md:w-1/3 py-1 px-2 max-w-xs focus:w-full focus:border focus:border-blue-500 outline-none ease-in-out duration-300"
+                        />
+                        {errors.username?.message && <ErrorValidationText error={errors.username.message}/>}
+                    </div>
+                    <div className="flex flex-col items-center w-full m-3">
+                        <label htmlFor="email">Correo electrónico:</label>
+                        <input type="email" id="email" placeholder="Email" 
+                            {...register('email', {
+                                    required: { value: true, message: 'Introduzca un correo electrónico'},
+                                    pattern: { value: EmailRegex, message: 'Introduzca un correo electrónico válido'}}
+                                )
+                            }
+                            className="border rounded-md shadow w-full md:w-1/3 py-1 px-2 max-w-xs focus:w-full focus:border focus:border-blue-500 outline-none ease-in-out duration-300"
+                        />
+                        {errors.email?.message && <ErrorValidationText error={errors.email.message}/>}
+                    </div>
+                    <div className="flex flex-col items-center w-full m-3">
+                        <label htmlFor="password">Contraseña:</label>
+                        <input type="password" id="password" placeholder="Contraseña" 
+                            {...register('password', {
+                                    required: {value: true, message: 'Introduzca una contraseña'},
+                                    minLength: {value: 6, message: 'La contraseña es demasiado corta'}}
+                                )
+                            }
+                            className="border rounded-md shadow w-full md:w-1/3 py-1 px-2 max-w-xs focus:w-full focus:border focus:border-blue-500 outline-none ease-in-out duration-300"
+                        />
+                        {errors.password?.message && <ErrorValidationText error={errors.password.message}/>}
+                    </div>
+                    <div className="flex flex-col items-center w-full mt-5">
+                        <button type="submit" className="px-3 py-2 mb-2 rounded-md bg-blue-500 text-white hover:bg-blue-400 hover:scale-105 hover:shadow-md transition duration-100 ease-in-out active:shadow-none">
+                            <h4 className="text-white font-bold">Registro</h4>
+                        </button>
+                        {loginError && <ErrorValidationText error={loginError} />}
+                    </div>
+                </form>
+            }
         </div>
     )
 }
---
+++ src/components/WelcomeUserComponent.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import { UserDataInterface } from "./RegistrationFormComponent";
+
+interface LoggedUserData {
+    data: UserDataInterface
+}
+
+export function WelcomeUserComponent({data}: LoggedUserData) {
+
+    return(
+        <>
+           <h3 className="text-xl font-semibold">
+               Bienvenido de nuevo, {data.username}
+            </h3>
+           <Link 
+            to={"/"} 
+            className="px-3 py-2 m-2 rounded-md bg-blue-500 text-white hover:bg-blue-400 hover:scale-105 hover:shadow-md transition duration-100 ease-in-out active:shadow-none">
+               Volver al inicio
+            </Link>
+        </>
+    )
+}
