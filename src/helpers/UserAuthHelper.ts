dd20ff2589df28fed1bd27d99a8503f1549b0431
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-19 10:56:59 +0000

    Improved helper function

--- src/helpers/UserAuthHelper.ts
+++ src/helpers/UserAuthHelper.ts
@@ -1,4 +1,9 @@
+import { getCookie } from "react-use-cookie";
 
 export function userIsAuth(user_id: string, token: string): boolean {
-    return (user_id !== "" && user_id.length >= 24 && token !== "");
+    const cookie_userId = getCookie('user_id');
+    const cookie_token = getCookie('token');
+    const user_check: boolean = (user_id === cookie_userId && user_id !== "");
+    const token_check: boolean = (token === cookie_token && token !== "");
+    return user_check && token_check;
 }
