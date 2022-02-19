ee04d8bb8d30b73e309786d35f30ae47f33deca6
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-19 11:21:51 +0000

    Fixed visualization

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -7,12 +7,8 @@ export const ResultsPage = () => {
                 <h1 className="text-4xl font-extrabold">Resultados y ranking</h1>
                 <h3 className="text-xl font-light">Aquí podrá visualizar todos los resultados de los métodos subidos por los usuarios</h3>
             </div>
-            <div className="overflow-x-auto overflow-y-auto w-[80%] sm:rounded-md sm:-mx-6 lg:-mx-8">
-                <div className="inline-block py-2 min-w-full">
-                    <div className="shadow-none md:drop-shadow-md">         
-                        <MethodsTableComponent />
-                    </div>
-                </div>
+            <div className="inline-block overflow-y-auto w-[80%] rounded-md">
+                <MethodsTableComponent />
             </div>
         </div>
     )
