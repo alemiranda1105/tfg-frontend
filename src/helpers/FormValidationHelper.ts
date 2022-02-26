f80e88ec91ed8ae4080681a9c6cef4073d7f467b
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-26 15:27:44 +0000

    HU-5 Form validation

--- src/helpers/FormValidationHelper.ts
+++ src/helpers/FormValidationHelper.ts
@@ -6,3 +6,23 @@ export const validateEmail = (email: string) => {
     return regex.test(email);
 }
 export const validatePassword = (password: string) => (password.length > 6);
+
+export const validateText = (text: string, maxLength?: number, minLength?: number, regex?: string) => {
+    let textLength = text.length;
+    if(regex) {
+        let regexp = new RegExp(regex);
+        if(!regexp.test(text)) {
+            return "Formato no válido";
+        }
+    }
+
+    if(maxLength && (textLength > maxLength)) {
+        return `El texto es demasiado largo, debe tener menos de ${maxLength} caracteres`;
+    }
+
+    if(minLength && (textLength < minLength)) {
+        return `El texto es demasiado corto, debe tener más de ${minLength} caracteres`;
+    }
+
+    return "";
+}
