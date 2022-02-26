730d3cad076af208fc299b1f291b37cec033c016
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-26 15:27:44 +0000

    HU-5 Form validation

--- src/components/MethodFormComponent.tsx
+++ src/components/MethodFormComponent.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 import React, { useContext, useState } from "react"
 import { AuthContext } from "../auth/AuthContextProvider";
+import { validateText } from "../helpers/FormValidationHelper";
 import { CustomInput } from "./CustomInput"
+import { ErrorValidationText } from "./ErrorValidationText";
 import { SubmitButton } from "./SubmitButton";
 
 interface NewMethodInterface {
@@ -27,10 +29,33 @@ export const MethodFormComponent = () => {
         results: []
     });
 
+    const [validationError, setValidationError] = useState({
+        info: "",
+        link: "",
+        name: "",
+        file: ""
+    });
+
     const [file, setFile] = useState<File>();
     
     const handleChange = (e: React.FormEvent<HTMLInputElement|HTMLTextAreaElement>) => {
         const {name, value} = e.currentTarget;
+
+        // Validation
+        var validation: string = "";
+        if(name === "name") {
+            validation = validateText(value, 25, 3);
+        } else if(name === "info") {
+            validation = validateText(value, 200, 5);
+        } else if(name === "link") {
+            validation = validateText(value, 50, 3);
+        }
+
+        setValidationError(prevState => ({
+            ...prevState,
+            [name]: validation
+        }));
+
         setMethodData(prevState => ({
             ...prevState,
             [name]: value
@@ -38,9 +63,16 @@ export const MethodFormComponent = () => {
     }
 
     const handleFileChange = (e: React.FormEvent<HTMLInputElement>) => {
-        const file = e.currentTarget.files!;
-        console.log(file);
-        setFile(file![0]);
+        if(e.currentTarget.files) {
+            const file = e.currentTarget.files;
+            console.log(file);
+            setFile(file[0]);
+        } else {
+            setValidationError(prevState => ({
+                ...prevState,
+                file: "Introduzca un archivo válido"
+            }));
+        }
     }
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -50,8 +82,6 @@ export const MethodFormComponent = () => {
             ...prevState,
             user_id: user_id,
         }));
-        console.log(methodData);
-        console.log(file);
         var formData = new FormData();
         formData.append('file', file!, `method_${user_id}.zip`);
         formData.append('data', JSON.stringify(methodData));
@@ -81,6 +111,7 @@ export const MethodFormComponent = () => {
                 <div className="flex flex-col items-center w-full m-3"> 
                     <label htmlFor="name">Nombre:</label>
                     <CustomInput type={"text"} name={"name"} placeholder={"Nombre"} handleChange={handleChange} required={true} />
+                    {validationError.name && <ErrorValidationText error={validationError.name}/>}
                 </div>
                 <div className="flex flex-col items-center w-full m-3"> 
                     <label htmlFor="info">Información:</label>
@@ -88,15 +119,18 @@ export const MethodFormComponent = () => {
                     onChange={handleChange}
                     className="border rounded-md shadow w-full md:w-1/3 py-1 px-2 max-w-xs focus:w-full focus:border focus:border-blue-500 outline-none ease-in-out duration-300"
                     name="info" id="info" cols={50} rows={10} placeholder="Información"></textarea>
+                    {validationError.info && <ErrorValidationText error={validationError.info}/>}
                 </div>
                 <div className="flex flex-col items-center w-full m-3"> 
                     <label htmlFor="link">Enlace a la publicación:</label>
                     <CustomInput type={"text"} name={"link"} placeholder={"Nombre"} handleChange={handleChange} required={true} />
+                    {validationError.link && <ErrorValidationText error={validationError.link}/>}
                 </div>
                 <div className="flex flex-col items-center w-full m-3"> 
                     <label htmlFor="file">Fichero con los resultados a comparar:</label>
                     <CustomInput type={"file"} name={"file"} accept={"zip,application/zip,application/x-zip,application/x-zip-compressed"} placeholder={""} handleChange={handleFileChange} required={true} />
                     <h6 className="text-sm font-light m-1">Solo se admiten ficheros en formato .zip</h6>
+                    {validationError.file && <ErrorValidationText error={validationError.file}/>}
                 </div>
 
                 <SubmitButton loginError={uploadError} text="Subir método"/>
