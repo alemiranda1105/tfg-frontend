afe6b3d41eb14882584dc336b86e878e7015e37c
Alejandro Miranda <amiranda110500@gmail.com>
2022-02-28 18:22:43 +0000

    Check if user is logged

--- src/pages/UploadMethodPage.tsx
+++ src/pages/UploadMethodPage.tsx
@@ -1,7 +1,19 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContextProvider";
 import { MethodFormComponent } from "../components/MethodFormComponent"
 
 
 export const UploadMethodPage = () => {
+    const { user_id, token } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(user_id === "" || token === "") {
+            navigate("/");
+        }
+    }, [user_id, token, navigate]);
+
     return(
         <div className="flex flex-col items-center text-center">
             <h1 className="text-3xl font-extrabold text-blue-700">Subir nuevo método</h1>
