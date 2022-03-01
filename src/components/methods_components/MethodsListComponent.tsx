b186fc4ebad8beec37c2671b8f3b5673a06281e0
Alejandro Miranda <amiranda110500@gmail.com>
2022-03-01 10:19:28 +0000

    HU-6 fetch user methods

--- src/components/methods_components/MethodsListComponent.tsx
+++ src/components/methods_components/MethodsListComponent.tsx
@@ -1,93 +1,59 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../auth/AuthContextProvider";
+import { useFetch } from "../../hooks/useFetch";
 import { MethodInterface } from "../table_components/MethodsTableComponent";
 
-interface MethodLinkProps {
-    method: MethodInterface
-}
-
-function DeleteButtonComponent({method}: MethodLinkProps) {
-    const handleClick = () => {
-        console.log("Eliminado " + method.id);
-    }
-    return (
-        <button className="px-3 py-2 m-2 rounded-md text-sm bg-red-500 text-white" onClick={handleClick}>
-            Borrar
-        </button>
-    )
-}
-
-function MethodLink({method}: MethodLinkProps) {
+export function MethodsListComponent() {
     const [showOptions, setShowOptions] = useState(true);
+    const {user_id, token} = useContext(AuthContext);
+    const {data, isPending, error} = useFetch<MethodInterface[]>(`methods/user_methods?user_id=${user_id}`, token, true);
 
     const toggleOptions = () => {
         setShowOptions(!showOptions);
     }
 
-    return(
-        <div className="flex flex-col m-3 p-2 w-full border rounded-md items-center hover:shadow-md hover:rounded-none hover:bg-slate-400/30 duration-300">
-            <div className="flex flex-row justify-between items-center w-full" onClick={toggleOptions}>
-                <div className="flex flex-col">
-                    <h3 className="font-bold">{method.name}</h3>
-                    <h4 className="font-light text-sm">{method.info.substring(0, 20)}...</h4>
-                </div>
-                <div className="m-2">
-                    <h4>{method.link}</h4>
-                </div>
-            </div>
-            {showOptions &&
-                <div className="flex flex-row">
-                    <Link to={`/method_details/${method.id}`} className="px-3 py-2 m-2 rounded-md text-sm bg-slate-500 text-white">Editar</Link>
-                    <Link to={`/method_details/${method.id}`} className="px-3 py-2 m-2 rounded-md text-sm bg-blue-500 text-white">Ver detalles</Link>
-                    <DeleteButtonComponent method={method} />
-                </div>
-            }
-        </div>
-    )
-}
+    const handleClick = (id: string) => {
+        console.log("Eliminado " + id);
+    }
 
-export function MethodsListComponent() {
-    const mockedMethodsList: MethodInterface[] = [
-        {
-            id: "1",
-            info: "Esto es un metodo de ejemplo",
-            link: "www.google.es",
-            name: "Metodo 1",
-            user_id: "1",
-            results: {
-                f1Score: 0.9899,
-                recallScore: 0.2223,
-                precisionScore: 0.3321
-            }
-        },
-        {
-            id: "2",
-            info: "Esto es un metodo de ejemplo",
-            link: "www.google.es",
-            name: "Metodo 2",
-            user_id: "1",
-            results: {
-                f1Score: 0.2323,
-                recallScore: 0.7687,
-                precisionScore: 0.6785
-            }
-        },
-        {
-            id: "3",
-            info: "Esto es un metodo de ejemplo",
-            link: "www.google.es",
-            name: "Metodo 3",
-            user_id: "1",
-            results: {
-                f1Score: 0.5743,
-                recallScore: 0.7857,
-                precisionScore: 0.8986
-            }
-        },
-    ]
     return (
         <div className="w-full">
-            {mockedMethodsList.map(method => <MethodLink method={method} key={method.id}/>)}
+            {isPending &&
+                <div className="flex flex-col items-center">
+                    <h3 className="animate-pulse text-2xl font-bold">Cargando...</h3>
+                </div>
+            }
+            {!isPending && error &&
+                <div className='flex flex-col items-center text-center'>
+                    <h3 className='text-2xl font-bold'>Ha ocurrido un error</h3>
+                    <p className='text-sm font-light'>Error: {error}</p>
+                </div>
+            }
+            {!isPending && data && data.map(method => {
+                return (
+                    <div className="flex flex-col m-3 p-2 w-full border rounded-md items-center hover:shadow-md hover:rounded-none hover:bg-slate-400/30 duration-300" key={method.id}>
+                        <div className="flex flex-row justify-between items-center w-full" onClick={toggleOptions}>
+                            <div className="flex flex-col">
+                                <h3 className="font-bold">{method.name}</h3>
+                                <h4 className="font-light text-sm">{method.info.substring(0, 20)}...</h4>
+                            </div>
+                            <div className="m-2">
+                                <h4>{method.link}</h4>
+                            </div>
+                        </div>
+                        {showOptions &&
+                            <div className="flex flex-row">
+                                <Link to={`/method_details/${method.id}`} className="px-3 py-2 m-2 rounded-md text-sm bg-slate-500 text-white">Editar</Link>
+                                <Link to={`/method_details/${method.id}`} className="px-3 py-2 m-2 rounded-md text-sm bg-blue-500 text-white">Ver detalles</Link>
+                                <button className="px-3 py-2 m-2 rounded-md text-sm bg-red-500 text-white" onClick={() => handleClick(method.id)}>
+                                    Borrar
+                                </button>
+                            </div>
+                        }
+                    </div>
+                )
+            })}
         </div>
     )
 }
