ed52c9862f9a8633dd8eb55aad8b6b045a83007c
Alejandro Miranda <amiranda110500@gmail.com>
2022-03-03 13:52:05 +0000

    HU-7 added form

--- src/pages/EditMethodPage.tsx
+++ src/pages/EditMethodPage.tsx
@@ -1,12 +1,15 @@
 import { useParams } from "react-router-dom";
+import { NewMethodFormComponent } from "../components/methods_components/NewMethodFormComponent";
 
 
 export function EditMethodPage() {
     const { methodId } = useParams();
 
     return (
-        <div>
-            Edit {methodId}
+        <div className="flex flex-col items-center text-center">
+            <h1 className="text-3xl font-extrabold text-blue-700">Editar método</h1>
+            <h3 className="text-md font-light md:m-0 m-3">En esta página deberá poner todos los datos para evaluar y comparar su método</h3>
+            {methodId && <NewMethodFormComponent methodId={methodId} withFile={true} />}
         </div>
     )
 }
