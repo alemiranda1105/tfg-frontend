f70a98e6a2fd491839575124cc50f0c9f8516f46
Alejandro Miranda <amiranda110500@gmail.com>
2022-03-03 18:36:39 +0000

    HU-7 Value error fixed

--- src/components/custom_components/CustomInput.tsx
+++ src/components/custom_components/CustomInput.tsx
@@ -13,7 +13,7 @@ interface InputProps {
 export function CustomInput({type, name, placeholder, required, value, accept, handleChange}: InputProps) {
     return(
         <>
-            <input type={type} name={name} placeholder={placeholder} id={name} onChange={handleChange} required={required} accept={accept} value={value}
+            <input type={type} name={name} placeholder={placeholder} id={name} onChange={handleChange} required={required} accept={accept} defaultValue={value}
             className="border rounded-md shadow w-full md:w-1/3 py-1 px-2 max-w-xs focus:w-full focus:border focus:border-blue-500 outline-none ease-in-out duration-300"
             />
         </>
