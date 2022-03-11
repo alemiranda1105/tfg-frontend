9f57b364fdcde729200648a218e235b6c1d66550
Alejandro Miranda <amiranda110500@gmail.com>
2022-03-11 10:11:01 +0000

    HU-16 data submitted to API

--- src/components/user_components/UpdateUserForm.tsx
+++ src/components/user_components/UpdateUserForm.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { EmailRegex } from "../../helpers/EmailRegex";
 import { validateText } from "../../helpers/FormValidationHelper";
 import { useFetch } from "../../hooks/useFetch";
@@ -10,10 +12,11 @@ import { UserProfileData } from "./UserDataComponent";
 
 
 interface UserForm {
-    user_id: string
+    user_id: string,
+    token: string
 }
 
-export const UpdateUserForm = ({user_id}: UserForm) => {
+export const UpdateUserForm = ({user_id, token}: UserForm) => {
     const {data: oldData, error, isPending} = useFetch<UserProfileData, undefined>(`users/profile?user_id=${user_id}`, 'GET');
     
     const [validationError, setValidationError] = useState({
@@ -25,7 +28,13 @@ export const UpdateUserForm = ({user_id}: UserForm) => {
         username: "",
         email: "",
         password: ""
-    })
+    });
+
+    // Submit state for check if there was any error or data were updated
+    const [submitState, setSubmitState] = useState({
+        updated: true,
+        error: ""
+    });
 
     useEffect(() => {
         if(oldData) {
@@ -57,13 +66,69 @@ export const UpdateUserForm = ({user_id}: UserForm) => {
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        console.table(userData);
+
+        let config = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        axios.put(`${process.env.REACT_APP_API_URL}/users/${user_id}`, userData, config)
+        .then(res => res.data)
+        .then(data => {
+            setUserData({
+                username: data.username,
+                email: data.email,
+                password: ""
+            });
+            setSubmitState({
+                updated: true,
+                error: ""
+            });
+        })
+        .catch(error => {
+            if(axios.isAxiosError(error)) {
+                setSubmitState({
+                    updated: false,
+                    error: error.response?.data.detail
+                });
+            } else {
+                setSubmitState({
+                    updated: false,
+                    error: "Algo ha ido mal, inténtelo de nuevo"
+                });
+            }
+        });
     }
 
     return (
         <div className="flex flex-col items-center w-3/4 p-4 rounded-md shadow-md bg-white">
             {
-                oldData &&
+                submitState.updated &&
+                <div className="flex flex-col items-center">
+                    <h2 className="text-lg font-bold m-2">Datos actualizados</h2>
+                    <h3 className="font-bold">Nombre de usuario</h3>
+                    <h4 className="font-light">{userData.username}</h4>
+                    <h3 className="font-bold">Correo electrónico</h3>
+                    <h4 className="font-light">{userData.email}</h4>
+                    <Link to="/profile" className="px-3 py-2 m-2 rounded-md bg-blue-500 text-white font-bold hover:bg-blue-400 transition duration-100 ease-in-out"> Volver al perfil</Link>
+                </div>
+            }
+            {
+                isPending &&
+                <div className="flex flex-col items-center">
+                    <h3 className="animate-pulse text-2xl font-bold">Cargando...</h3>
+                </div>
+            }
+            {
+                error && !isPending &&
+                <div className='flex flex-col items-center text-center'>
+                    <h3 className='text-lg'>Ha ocurrido un error</h3>
+                    <p className='text-sm font-light'>Error: {error}</p>
+                </div>
+            }
+            {
+                oldData && !isPending && !submitState.updated &&
                 <form className="flex flex-col items-center w-full" onSubmit={handleSubmit}>
                     <div className="flex flex-col items-center w-full m-3"> 
                         <label htmlFor="name">Nombre de usuario:</label>
@@ -79,7 +144,7 @@ export const UpdateUserForm = ({user_id}: UserForm) => {
                         <label htmlFor="link">Contraseña actual:</label>
                         <CustomInput type={"password"} name={"password"} placeholder={"Contraseña"} handleChange={handleChange} required={true} />
                     </div>
-                    <SubmitButton loginError={""} text={"Actualizar usuario"} />
+                    <SubmitButton loginError={submitState.error} text={"Actualizar usuario"} />
                 </form>
             }
         </div>
--- src/pages/UpdateUserProfilePage.tsx
+++ src/pages/UpdateUserProfilePage.tsx
@@ -1,15 +1,18 @@
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { AuthContext } from "../auth/AuthContextProvider";
 import { UpdateUserForm } from "../components/user_components/UpdateUserForm";
 
 
 export function UpdateUserProfilePage() {
     const { user_id } = useParams();
+    const { token } = useContext(AuthContext);
     
     return(
         <div className="flex flex-col items-center text-center">
             <h1 className="text-3xl font-extrabold text-blue-700">Editar usuario</h1>
             <h3 className="text-md font-light md:m-0 m-3">Edite los datos del usuario</h3>
-            { user_id && <UpdateUserForm user_id={user_id}/> }
+            { user_id && token && <UpdateUserForm user_id={user_id} token={token}/> }
         </div>
     )
 }
