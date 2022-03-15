be6cfef1aa7ea813497df30eff08b83c2052c16b
Alejandro Miranda <amiranda110500@gmail.com>
2022-03-15 18:07:45 +0000

    HU-10 mocked FAQ page

--- src/pages/FAQPage.tsx
+++ src/pages/FAQPage.tsx
@@ -3,10 +3,31 @@ import { descriptionText } from "../mock/MockedTextPresentation";
 
 export function FAQPage() {
     return (
-        <div>
+        <div className="flex flex-col justify-between items-center">
+            <h1 className="text-3xl font-extrabold text-blue-700">Preguntas frecuentes</h1>
             <section className="mx-5 my-2">
                 <h2 className="text-2xl font-bold tracking-wide leading-10 text-blue-700">
-                    Descripción
+                    Pregunta 1
+                </h2>
+                <div className="p-3 rounded-md border bg-white">
+                    <p>
+                        {descriptionText}
+                    </p>
+                </div>
+            </section>
+            <section className="mx-5 my-2">
+                <h2 className="text-2xl font-bold tracking-wide leading-10 text-blue-700">
+                    Pregunta 2
+                </h2>
+                <div className="p-3 rounded-md border bg-white">
+                    <p>
+                        {descriptionText}
+                    </p>
+                </div>
+            </section>
+            <section className="mx-5 my-2">
+                <h2 className="text-2xl font-bold tracking-wide leading-10 text-blue-700">
+                    Pregunta 3
                 </h2>
                 <div className="p-3 rounded-md border bg-white">
                     <p>
