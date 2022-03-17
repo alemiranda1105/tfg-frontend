69a98dfa3c6ee4f34e6710a49b790a267a101a39
Alejandro Miranda <amiranda110500@gmail.com>
2022-03-17 13:54:40 +0000

    Added link to registration

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 import { LoginFormComponent } from "../components/auth_components/LoginFormComponent"
 
 
@@ -6,6 +7,7 @@ export const LoginPage = () => {
         <div className="flex flex-col items-center">
             <h1 className="text-3xl font-extrabold text-blue-700">Iniciar sesión</h1>
             <LoginFormComponent />
+            <Link to="/signup" className="font-light text-lg m-2 hover:text-blue-700">Regístrese aquí</Link>
         </div>
     )
 }
