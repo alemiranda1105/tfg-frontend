7508f8b741f3412ab491c9e2036f65e43b516874
Alejandro Miranda <amiranda110500@gmail.com>
2022-04-24 11:58:59 +0100

    Dataset download page updated

--- src/components/download_components/DownloadButton.tsx
+++ src/components/download_components/DownloadButton.tsx
@@ -21,9 +21,9 @@ export const DownloadButton = ({text, url, fileType}: DownloadButtonProps) => {
     return (
         <button
         onClick={() => handleClick()}
-        className="px-3 py-2 mb-2 bg-blue-500 rounded-lg hover:scale-110 hover:rounded-none transition duration-300 ease-in"
+        className="px-3 py-2 mb-2 border bg-blue-300 text-white rounded-lg hover:scale-x-110 hover:rounded-none transition duration-300 ease-linear"
         >
-            <h6 className="font-bold text-2xl text-white">
+            <h6 className="font-bold text-2xl">
                 {text}
             </h6>
         </button>
--- src/pages/DownloadDatasetPage.tsx
+++ src/pages/DownloadDatasetPage.tsx
@@ -1,8 +1,4 @@
-
-import { DownloadButton } from '../components/download_components/DownloadButton';
 import { characteristicsText } from '../mock/MockedTextPresentation';
-
-import '../styles/DownloadDatasetPage.css';
 
 import folder_img from '../res/dataset/folders.png';
 import result_img from '../res/dataset/result-files.png';
@@ -10,11 +6,11 @@ import result_img from '../res/dataset/result-files.png';
 export function DownloadDatasetPage() {
     return(
         <div className='flex flex-col justify-center items-center text-center'>
-            <h1 className="title">
+            <h1 className="section-title">
                 Download our dataset
             </h1>
             <div className='flex flex-col items-center w-3/4 bg-white rounded m-2 p-3'>
-                <h3 className='small-title'>Information</h3>
+                <h3 className='section-small-title'>Information</h3>
                 <p className='font-lg m-1 p-1 text-justify'>
                     {characteristicsText}
                 </p>
@@ -26,7 +22,19 @@ export function DownloadDatasetPage() {
                     </p>
                     <img src={result_img} alt="Ejemplo de archivos" className='rounded m-3 w-2/3 shadow active:scale-150 transition-all duration-500 ease-in-out' />
                 </div>
-                <DownloadButton text={'Download'} url={'dataset'} fileType={'application/x-zip-compressed'} />
+                <div className='m-3 w-full border rounded flex flex-col'>
+                    <h3 className='font-semibold text-lg'>Enlaces de descarga</h3>
+                    <a
+                    className='underline hover:text-blue-500 hover:font-semibold transition duration-300 p-2'
+                    href="https://alumnosulpgc-my.sharepoint.com/:u:/g/personal/jsanchez_ulpgc_es/EVzjDUXQgupFpMhq6ERMFd4BMVhQYhRTNG7dHsUHgH7BZQ">
+                        OneDrive
+                    </a>
+                    <a
+                    className='underline hover:text-blue-500 hover:font-semibold transition duration-300 p-2'
+                    href="https://zenodo.org/record/6373179/files/idsem.zip?download=1">
+                        Zenodo
+                    </a>
+                </div>
             </div>
         </div>
     )
