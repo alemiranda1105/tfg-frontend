bce74758e9e8f7ca3b018c4ce461cf2913c8bfa5
Alejandro Miranda <amiranda110500@gmail.com>
2022-04-28 14:18:05 +0100

    Texts updated

--- src/pages/DownloadDatasetPage.tsx
+++ src/pages/DownloadDatasetPage.tsx
@@ -4,6 +4,9 @@ import { characteristicsText, fieldDescription } from '../mock/MockedTextPresent
 import folder_img from '../res/dataset/folders.png';
 import result_img from '../res/dataset/result-files.png';
 
+import test_dataset from '../res/dataset/test-dataset.png'
+import training_dataset from '../res/dataset/training-dataset.png'
+
 export function DownloadDatasetPage() {
     const [show, setShow] = useState(false);
 
@@ -16,18 +19,38 @@ export function DownloadDatasetPage() {
             <h1 className="section-title">
                 Download our dataset
             </h1>
-            <div className='flex flex-col items-center w-3/4 bg-white rounded m-2 p-3'>
+            <div className='flex flex-col items-center sm:w-full md:w-1/2 bg-white rounded m-2 p-3'>
                 <h3 className='section-small-title'>Information</h3>
                 <p className='font-lg m-1 p-1 text-justify'>
-                    {characteristicsText}
+                    El dataset que ofrecemos a los investigadores se encuentra divido en <strong>9 clases</strong>, cada una de estas clases está compuesta con <strong>5000 PDF</strong> 
+                    de facturas eléctricas con todos los campos a evaluar. <br />
+                    Además, se ofrece un dataset de entrenamiento para que puedas probar tus métodos antes de realizar la evaluación real y subir los resultados a la plataforma.
+                    Este conjunto de tests, se encuentra divido en <strong>6 clases</strong> con <strong>5000 PDF</strong> y otros <strong>5000 JSON</strong> para que puedas comprobar
+                    los resultados esperados en cada caso por cada campo de manera sencilla. <br />
+                    Al final de esta página, podrás encontrar que significa cada campo.
+                </p>
+                <div className="flex justify-evenly content-evenly w-full m-2">
+                    <div className="w-full flex flex-col justify-center content-center items-center">
+                        <img src={test_dataset} alt="Test dataset example" className='rounded m-3 w-3/4 shadow active:scale-150 transition-all duration-500 ease-in-out' />
+                        <h4 className='font-light'>Test dataset</h4>
+                    </div>
+                    <div className="w-full flex flex-col justify-center content-center items-center">
+                        <img src={training_dataset} alt="Training dataset example" className='rounded m-3 w-3/4 shadow active:scale-150 transition-all duration-500 ease-in-out' />
+                        <h4 className='font-light'>Training dataset</h4>
+                    </div>
+                </div>
+                <h3 className='section-small-title'>Results</h3>
+                <p className='font-lg m-1 p-1 text-justify'>
+                    Una vez ejecutado el método de extracción, deberá obtener las carpetas en el siguiente formato con los 
+                    resultados con el fin de que la evaluación sea exitosa.
                 </p>
                 <div className='flex flex-col items-center text-justify'>
-                    <img src={folder_img} alt="carpetas de los resultados" className='rounded m-3 w-2/3 shadow active:scale-150 transition-all duration-500 ease-in-out' />
+                    <img src={folder_img} alt="carpetas de los resultados" className='rounded m-3 w-1/2 shadow active:scale-150 transition-all duration-500 ease-in-out' />
                     <p>
-                        The results must be inside folders as it can be seen in the image above to allow our platform to evaluate them.
-                        Inside each folder, files with <code>.json</code> extension must be found with the obtained results in each category. For example:
+                        Dentro de cada carpeta, los resultados estarán ordenados de la siguiente manera y en formato <code>.json</code> para que sean
+                        leídos y procesados correctamente.
                     </p>
-                    <img src={result_img} alt="Ejemplo de archivos" className='rounded m-3 w-2/3 shadow active:scale-150 transition-all duration-500 ease-in-out' />
+                    <img src={result_img} alt="Ejemplo de archivos" className='rounded m-3 w-1/2 shadow active:scale-150 transition-all duration-500 ease-in-out' />
                 </div>
                 <h3 className='section-small-title'>Field's meaning</h3>
                 <p>If you press in the next button, the defintion of each field will appear</p>
@@ -46,14 +69,14 @@ export function DownloadDatasetPage() {
                 </button>
                 {
                     show &&
-                    <div className="text-left border rounded p-1 bg-slate-100">
+                    <div className="text-left border rounded p-1 bg-slate-100 w-full overflow-scroll">
                         <pre>
                             {fieldDescription}
                         </pre>
                     </div>
                 }
 
-                <div className='m-3 w-full border rounded flex flex-col'>
+                <div className='m-3 w-full md:w-1/3 border rounded flex flex-col'>
                     <h3 className='font-semibold text-lg'>Download links</h3>
                     <a
                     className='underline hover:text-blue-500 hover:font-semibold transition duration-300 p-2'
--- src/pages/FAQPage.tsx
+++ src/pages/FAQPage.tsx
@@ -6,33 +6,38 @@ export function FAQPage() {
         <div className="flex flex-col items-center w-full">
             <div className="flex flex-col justify-between items-center w-3/4">
                 <h1 className="section-title">FAQ</h1>
-                <section className="mx-5 my-2">
+                <section className="mx-5 my-4">
                     <h2 className="section-small-title">
-                        Question 1
+                        ¿Cuál es la finalidad de la plataforma?
                     </h2>
-                    <div className="p-3 rounded-md border bg-white">
+                    <div className="p-3 border-b-2 border-black bg-white">
                         <p>
-                            {descriptionText}
+                            La finalidad de esta plataforma es ofrecer a los investigadores un sitio único donde poder comparar los métodos de extracción de datos de facturas eléctricas que han desarrollado con los de otros investigadores
+                            gracias al sistema de puntuaciones que posee la plataforma. <br />
+                            Además, estos resultados podrán ser incluidos en un artículo, si así lo desea. <br />
+                            <i>Desde la plataforma, pedimos a los investigadores que la usen que esta sea citada.</i>
                         </p>
                     </div>
                 </section>
-                <section className="mx-5 my-2">
+                <section className="mx-5 my-4">
                     <h2 className="section-small-title">
-                        Question 2
+                        ¿Es necesario que mis resultados sean públicos?
                     </h2>
-                    <div className="p-3 rounded-md border bg-white">
+                    <div className="p-3 border-b-2 border-black bg-white">
                         <p>
-                            {descriptionText}
+                            No, no lo es, a la hora de subirlos puedes seleccionar que sean privados, es decir, solo tú los puedes ver; anónimo, otros usuarios ven los resultados pero no su autor, y públicos, todos los usuarios pueden ver todos los datos que hayas añadido. <br />
+                            En caso de que cambies de idea, puedes editar la privacidad del método desde tu perfil o la página de tus métodos.
                         </p>
                     </div>
                 </section>
-                <section className="mx-5 my-2">
+                <section className="mx-5 my-4">
                     <h2 className="section-small-title">
-                        Question 3
+                        ¿Existe algún límite de subida?
                     </h2>
-                    <div className="p-3 rounded-md border bg-white">
+                    <div className="p-3 border-b-2 border-black bg-white">
                         <p>
-                            {descriptionText}
+                            No, la plataforma no dispone de ningún límite de subida, sin embargo, pedimos por favor a los usuarios que solo suban métodos que estén terminados y no usen la plataforma como un conjunto de tests ya que nuestros recursos son limitados y de esta manera puedes
+                            perjudicar a otros usuarios que hagan un correcto uso de esta.
                         </p>
                     </div>
                 </section>
