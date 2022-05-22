f09b89a3f3b29790703769f291ac2c52f5c9bf62
Alejandro Miranda <amiranda110500@gmail.com>
2022-05-22 19:47:41 +0100

    Login button changed

--- src/components/navbar/NavigationBarButton.tsx
+++ src/components/navbar/NavigationBarButton.tsx
@@ -6,10 +6,18 @@ export const NavigationBarButton = ({name, url, actual}: LinkDict) => {
     const [style, setStyle] = useState("font-bold p-2.5 mx-3 my-1 hover:bg-white hover:rounded hover:shadow-inner duration-150");
 
     useEffect(() => {
+        let newStyle = style;
         if(actual) {
-            setStyle("font-extrabold p-2.5 mx-3 my-1 text-black bg-slate-200 rounded hover:rounded-none hover:shadow-inner hover:bg-white duration-150");
+            newStyle = "font-extrabold p-2.5 mx-3 my-1 text-black bg-slate-200 rounded hover:rounded-none hover:shadow-inner hover:bg-white duration-150"
         }
-    }, [actual])
+        if(name === "Login") {
+            newStyle += " rounded-lg bg-blue-300 hover:bg-white"
+        }
+        if(name === "Sign up") {
+            newStyle += " rounded-lg border-2 border-blue-500 hover:bg-blue-500"
+        }
+        setStyle(newStyle)
+    }, [actual, name, style])
 
     return(
         <>
