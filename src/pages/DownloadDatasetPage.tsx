2320a3036805da0dcaa42fcd6e73e33bc358cd9f
Alejandro Miranda <amiranda110500@gmail.com>
2022-05-23 19:06:09 +0100

    download fields meaning

--- src/pages/DownloadDatasetPage.tsx
+++ src/pages/DownloadDatasetPage.tsx
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { ContentListComponent } from '../components/content_components/ContentListComponent';
 import { DownloadLinksComponent } from '../components/download_components/DownloadLinksComponent';
 import { fieldDescription } from '../mock/MockedTextPresentation';
+import { fieldDescription as fieldsJson } from '../interface/FieldDescription';
+
+import { HiOutlineDocumentDownload } from 'react-icons/hi'
 
 import folder_img from '../res/dataset/folders.png';
 import result_img from '../res/dataset/result-files.png';
@@ -15,6 +18,17 @@ export function DownloadDatasetPage() {
     const showClick = () => {
         setShow(!show)
     }
+
+    const exportData = () => {
+        const jsonString = `data:text/json;chatset=utf-8,${encodeURIComponent(
+          JSON.stringify(fieldsJson)
+        )}`;
+        const link = document.createElement("a");
+        link.href = jsonString;
+        link.download = "fields_meaning.json";
+    
+        link.click();
+    };
 
     return (
         <div className='flex flex-col justify-center items-center text-center'>
@@ -68,6 +82,14 @@ export function DownloadDatasetPage() {
                 <section className="flex flex-col items-center w-3/4 p-2.5 m-2">
                     <h3 className='section-small-title'>Field's meaning</h3>
                     <p>If you press in the next button, the defintion of each field will appear</p>
+                    <p>And also, you can download the meaning of the fields pressing the download button</p>
+                    <button
+                    className='flex items-center p-2 m-1 mb-3 border rounded bg-blue-200 font-bold shadow hover:shadow-none'
+                    onClick={exportData}
+                    >
+                        <HiOutlineDocumentDownload size={22} />
+                        Download JSON
+                    </button>
                     <button
                     className='p-2 m-1 border rounded bg-slate-200 font-bold shadow hover:shadow-none'
                     onClick={() => showClick()}
